4fd7e79eb6aad310dbced5b0d5d42af0b358ff91
Kritika Umashankar <kritikaumashankar@gmail.com>
2018-09-08 14:57:35 -0600

    changed userbio.rb

--- app/models/user_bio.rb
+++ app/models/user_bio.rb
@@ -1,11 +1,3 @@
 class UserBio < ApplicationRecord
-  validate :one_bio_per_user
-
-  private
-
-  def one_bio_per_user
-     if current_user.user_bio && current_user.user_bio != self
-       errors.add(:current_user, "already has a bio")
-     end
-  end
+  belongs_to :user
 end
